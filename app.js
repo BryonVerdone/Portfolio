ffa60563d8aa96def8a2c375dfac58015fea49b9
BryonVerdone <bryonverdone@gmail.com>
2022-01-12 20:42:34 -0500

    refactored close mobile nav function

--- app.js
+++ app.js
@@ -2,10 +2,18 @@
 
 const mobileBtn = document.getElementById('mobile-btn');
 const mobileNav = document.getElementById('mobile-nav');
-mobileBtn.addEventListener('click', function () {
+mobileBtn.addEventListener('click', closeMobileNav);
+
+function closeMobileNav() {
   if (mobileNav.classList.contains('mobile-hidden')) {
     mobileNav.classList.toggle('mobile-show');
   }
-});
+}
 
 //close navbar on mobile link click
+
+const mobileLink = document.querySelectorAll('.mobile-item');
+
+mobileLink.forEach((link) => {
+  link.addEventListener('click', closeMobileNav);
+});
